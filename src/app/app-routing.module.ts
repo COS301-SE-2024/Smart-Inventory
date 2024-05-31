f3728c9d23d1ba4ecf1796ac02227da1af0f4511
Thabang Kgaladi <u21686875@tuks.co.za>
2024-05-31 23:44:47 +0200

    Updated help page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     path: 'settings',
     loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
   },
+  {
+    path: 'help',
+    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
+  },
 
 ];
 

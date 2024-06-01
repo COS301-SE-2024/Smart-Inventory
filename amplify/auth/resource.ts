87c4a06cde285ffd998c7bd1b3dfa96ceb8b3d34
Bouchaib <u20420740@tuks.co.za>
2024-06-01 22:40:47 +0200

    feat(signup):added registered users to admin user group

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,5 @@
 import { defineAuth } from '@aws-amplify/backend';
-
+import { postConfirmation } from "./post-confirmation/resource"
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
@@ -19,4 +19,11 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  groups: ["admin"],
+  triggers: {
+    postConfirmation,
+  },
+  access: (allow) => [
+    allow.resource(postConfirmation).to(["addUserToGroup"]),
+  ],
 });

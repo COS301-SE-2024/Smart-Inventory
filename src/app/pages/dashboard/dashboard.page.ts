1f95f8cc9b119cda06b2aeed730862eca6c24a71
Thabang Kgaladi <u21686875@tuks.co.za>
2024-06-01 18:15:22 +0200

    Add member working

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,23 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, IonModal } from '@ionic/angular';
 
-@Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss'],
-})
-export class DashboardPage implements OnInit {
-  userName: string = 'John Doe'; // Example user name
-  userRole: string = 'Admin'; // Example user role
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  showUserProfile() {
-    // Logic to show user profile
-    console.log('User profile clicked');
-  }
+interface TeamMember {
+	id: number;
+	name: string;
+	role: string;
 }
 
+@Component({
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.page.html',
+	styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+	userName: string = 'John Doe';
+	userRole: string = 'Admin';
+	members: TeamMember[] = [
+		{ id: 1, name: 'Alice Johnson', role: 'Analyst' },
+		{ id: 2, name: 'Bob White', role: 'Engineer' },
+		{ id: 3, name: 'Carol Blue', role: 'Manager' }
+	];
+	filteredMembers: TeamMember[] = [...this.members];
+	teamMembers: TeamMember[] =[] ; // List to display in the dashboard
+	searchQuery: string = '';
 
+	constructor(private modalController: ModalController) { }
+
+	ngOnInit() { }
+
+	addMember(member: TeamMember) {
+		this.teamMembers.push(member);
+		console.log('Member added:', member);
+	}
+
+	selectMember(member: TeamMember) {
+		this.teamMembers.push(member);
+		this.modalController.dismiss();
+		console.log('Member added:', member);
+	}
+
+	removeMember(memberToRemove: TeamMember) {
+		this.teamMembers = this.teamMembers.filter(member => member.id !== memberToRemove.id);
+	}
+
+	showUserProfile() {
+		console.log('wassgood');
+	}
+}

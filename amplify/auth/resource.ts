dabc9c86643dd44784f5f41c8345cbf745c98281
Bouchaib <u20420740@tuks.co.za>
2024-06-01 18:15:58 +0200

    Added firstname and family name to sign up form

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,6 +5,17 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
+  userAttributes: {
+    givenName: {
+      mutable: true,
+      required: true,
+    },
+    familyName: {
+      mutable: true,
+      required: true,
+    },
+  },
+
   loginWith: {
     email: true,
   },

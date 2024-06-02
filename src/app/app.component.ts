3e68761c785577ac398359eb263a7229d1c59350
Thabang Kgaladi <u21686875@tuks.co.za>
2024-06-02 18:22:52 +0200

    Fixed side nav now showing in the relevant pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-    constructor() {}
+export class AppComponent implements OnDestroy {
+    private subscription: Subscription;
+    showSidebar: boolean = false;
+
+    constructor(private router: Router) {
+        this.subscription = this.router.events.pipe(
+            filter((event: any) => event instanceof NavigationEnd)
+        ).subscribe((event: NavigationEnd) => {
+            this.showSidebar = event.url !== '/';
+        });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }

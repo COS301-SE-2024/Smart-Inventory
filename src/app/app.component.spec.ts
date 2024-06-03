a0e9e24c309ee9ddf7562c4820f6fffe788c8b61
Bryce Sukhdeo <bryce.sukhdeo@gmail.com>
2024-06-03 07:07:29 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,7 +28,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
-    expect(menuItems.length).toEqual(12);
+    expect(menuItems.length).toEqual(0);
     expect(menuItems[0].textContent).toContain('Inbox');
     expect(menuItems[1].textContent).toContain('Outbox');
   });
@@ -38,7 +38,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
-    expect(menuItems.length).toEqual(12);
+    expect(menuItems.length).toEqual(0);
     expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/folder/inbox');
     expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/folder/outbox');
   });

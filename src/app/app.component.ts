0fd6bf91160bab1d437cf64141b4e8aaab720e13
tristanU21442615 <u21442615@tuks.co.za>
2024-06-10 10:29:54 +0200

    feat/ordersv2: Update styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  AmplifyAuthenticatorModule,
-  AuthenticatorService,
-} from '@aws-amplify/ui-angular';
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { fetchAuthSession } from 'aws-amplify/auth';
@@ -13,36 +10,30 @@ import { GridComponent } from './components/grid/grid.component';
 Amplify.configure(outputs);
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [
-    RouterOutlet,
-    GridComponent,
-    AmplifyAuthenticatorModule,
-    SidebarComponent,
-    HeaderComponent,
-  ],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, GridComponent, AmplifyAuthenticatorModule, SidebarComponent, HeaderComponent],
 })
 export class AppComponent implements OnInit {
-  title = 'Smart-Inventory';
-  sidebarCollapsed = false;
+    title = 'Smart-Inventory';
+    sidebarCollapsed = false;
 
-  constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure(outputs);
-  }
-
-  ngOnInit() {
-    this.logAuthSession();
-  }
-
-  async logAuthSession() {
-    try {
-      const session = await fetchAuthSession();
-      console.log(session);
-    } catch (error) {
-      console.error('Error fetching auth session:', error);
+    constructor(public authenticator: AuthenticatorService) {
+        Amplify.configure(outputs);
     }
-  }
+
+    ngOnInit() {
+        this.logAuthSession();
+    }
+
+    async logAuthSession() {
+        try {
+            const session = await fetchAuthSession();
+            console.log(session);
+        } catch (error) {
+            console.error('Error fetching auth session:', error);
+        }
+    }
 }

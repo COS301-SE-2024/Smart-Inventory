6f503b21d852c06633b912c3db986794fe98eab2
tristanU21442615 <u21442615@tuks.co.za>
2024-06-11 16:21:00 +0200

    feat/bug: fix git issue

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,50 +6,49 @@ import { CommonModule } from '@angular/common';
 import { MatSidenav } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [MaterialModule, CommonModule, RouterLink, FormsModule],
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [MaterialModule, CommonModule, RouterLink, FormsModule],
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-  isExpanded = false;
-  sidebarWidth = 300; // Default sidebar width
-  menuItems = [
-    { label: 'Dashboard', icon: 'home' , routerLink: '/dashboard'},
-    { label: 'Inventory', icon: 'inventory_2', routerLink: "/inventory" },
-    { label: 'Reports', icon: 'assessment', routerLink: "/reports" },
-    { label: 'Requests', icon: 'request_quote', routerLink: "/requests" },
-    { label: 'Suppliers', icon: 'local_shipping', routerLink: "/suppliers" },
-    { label: 'Orders', icon: 'shopping_cart', routerLink: "/orders" },
-    { label: 'Settings', icon: 'settings', routerLink: '/settings'},
-    { label: 'Log Out', icon: 'exit_to_app', click: true }
-  ];
-  constructor(private router: Router) { }
+    @ViewChild('sidenav') sidenav!: MatSidenav;
+    isExpanded = false;
+    sidebarWidth = 300; // Default sidebar width
+    menuItems = [
+        { label: 'Dashboard', icon: 'dashboard', routerLink: '/dashboard' },
+        { label: 'Inventory', icon: 'inventory_2', routerLink: '/inventory' },
+        { label: 'Reports', icon: 'assessment', routerLink: '/reports' },
+        { label: 'Team', icon: 'people', routerLink: '/team' },
+        { label: 'Suppliers', icon: 'local_shipping', routerLink: '/suppliers' },
+        { label: 'Orders', icon: 'assignment', routerLink: '/orders' },
+        { label: 'Settings', icon: 'settings', routerLink: '/settings' },
+        { label: 'Log Out', icon: 'exit_to_app', click: true },
+    ];
+    constructor(private router: Router) {}
 
-  toggle(item: any): void {
-    if (item.submenu) {
-      item.expanded = !item.expanded;
+    toggle(item: any): void {
+        if (item.submenu) {
+            item.expanded = !item.expanded;
+        }
     }
-  }
 
-  originalWidth: string = '200px';  // Original width of the sidenav
-  expandedWidth: string = '300px';  // Expanded width of the sidenav
+    originalWidth: string = '200px'; // Original width of the sidenav
+    expandedWidth: string = '300px'; // Expanded width of the sidenav
 
-  // This method toggles the sidenav and changes its width
-  toggleSidenav() {
-    this.isExpanded = !this.isExpanded;  // Toggle the expansion state
-    // this.sidenav.toggle();  // Toggle the visibility of the sidenav
-  }
-
-
-  async signOut() {
-    try {
-      await signOut();
-      this.router.navigate(['/login']);
-    } catch (error) {
-      console.error('Error signing out:', error);
+    // This method toggles the sidenav and changes its width
+    toggleSidenav() {
+        this.isExpanded = !this.isExpanded; // Toggle the expansion state
+        // this.sidenav.toggle();  // Toggle the visibility of the sidenav
     }
-  }
+
+    async signOut() {
+        try {
+            await signOut();
+            this.router.navigate(['/login']);
+        } catch (error) {
+            console.error('Error signing out:', error);
+        }
+    }
 }
--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,46 +1,46 @@
 import { Component } from '@angular/core';
 import { ColDef } from 'ag-grid-community';
 import { GridComponent } from '../../components/grid/grid.component';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
+import { TitleService } from '../../components/header/title.service';
 
 @Component({
-  selector: 'app-orders',
-  standalone: true,
-  imports: [GridComponent, MatButtonModule],
-  templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+    selector: 'app-orders',
+    standalone: true,
+    imports: [GridComponent, MatButtonModule],
+    templateUrl: './orders.component.html',
+    styleUrl: './orders.component.css',
 })
 export class OrdersComponent {
+    constructor(private titleService: TitleService) {}
 
-  addButton: any = {text: 'Orders', button: true}
+    addButton: any = { text: 'Orders', button: true };
 
-  // Row Data: The data to be displayed.
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
-    { make: 'Fiat', model: '500', price: 15774, electric: false },
-    { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
-  ];
+    // Row Data: The data to be displayed.
+    rowData = [
+        { Order_ID: 'Tesla', Date: 'Model Y', Supplier: 64950, Items: 10 },
+        { Order_ID: 'Ford', Date: 'F-Series', Supplier: 33850, Items: 4 },
+        { Order_ID: 'Toyota', Date: 'Corolla', Supplier: 29600, Items: 5 },
+        { Order_ID: 'Mercedes', Date: 'EQA', Supplier: 48890, Items: 2 },
+        { Order_ID: 'Fiat', Date: '500', Supplier: 15774, Items: 1 },
+        { Order_ID: 'Nissan', Date: 'Juke', Supplier: 20675, Items: 7 },
+    ];
 
-  // Column Definitions: Defines & controls grid columns.
-  colDefs: any[] = [
-    { elementType: 'input', field: "make", headerName: 'Type', filter: 'agSetColumnFilter'},
-    { elementType: 'input', field: "model", filter: 'agSetColumnFilter' },
-    { elementType: 'input', field: "price", filter: 'agSetColumnFilter' },
-    { elementType: 'input', field: "electric", filter: 'agSetColumnFilter' }
-  ];
-  
-  defaultColDef: ColDef = {
-    flex: 1,
-  }
+    // Column Definitions: Defines & controls grid columns.
+    colDefs: any[] = [
+        { elementType: 'input', field: 'Order_ID', filter: 'agSetColumnFilter' },
+        { elementType: 'input', field: 'Date', filter: 'agSetColumnFilter' },
+        { elementType: 'input', field: 'Supplier', filter: 'agSetColumnFilter' },
+        { elementType: 'input', field: 'Items', filter: 'agSetColumnFilter' },
+    ];
 
+    defaultColDef: ColDef = {
+        flex: 1,
+    };
 
+    openAddDialog() {}
 
-  openAddDialog(){
-
-  }
-
+    ngOnInit() {
+        this.titleService.updateTitle('Orders');
+    }
 }

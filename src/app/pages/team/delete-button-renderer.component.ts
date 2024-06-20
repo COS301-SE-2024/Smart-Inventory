46525e743536f1eb652d947281358fb17ac5f554
Bouchaib <u20420740@tuks.co.za>
2024-06-20 17:56:00 +0200

    feat(team): Updated delete button styles

--- src/app/pages/team/delete-button-renderer.component.ts
+++ src/app/pages/team/delete-button-renderer.component.ts
@@ -3,29 +3,46 @@ import { ICellRendererParams } from 'ag-grid-community';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-delete-button-renderer',
-  standalone: true,
-  template: `<button class="delete-button" (click)="onDeleteButtonClick()">Delete</button>`,
-  styles: [`
-    .delete-button {;
-      font-size: 12px;
+    selector: 'app-delete-button-renderer',
+    standalone: true,
+    template: `<button class="delete-button" (click)="onDeleteButtonClick()">Delete</button>`,
+    styles: [
+        `
+    .delete-button {
+      background-color: #E0E0E0;
+      color: #616161;            
+      border: none;              
+      padding: 8px 12px;         
+      border-radius: 4px;        
+      cursor: pointer;          
+      transition: background-color 0.3s ease; 
+      line-height: 1.2;
     }
-  `]
+
+    .delete-button:hover {
+      background-color: #BDBDBD; /* Slightly darker grey on hover */
+    }
+
+    .delete-button:active {
+      background-color: #757575; /* Even darker grey when clicked */
+    }
+        `,
+    ],
 })
 export class DeleteButtonRenderer implements ICellRendererAngularComp {
-  private params!: ICellRendererParams;
+    private params!: ICellRendererParams;
 
-  agInit(params: ICellRendererParams): void {
-    this.params = params;
-  }
+    agInit(params: ICellRendererParams): void {
+        this.params = params;
+    }
 
-  refresh(params: ICellRendererParams): boolean {
-    return true;
-  }
+    refresh(params: ICellRendererParams): boolean {
+        return true;
+    }
 
-  onDeleteButtonClick() {
-    const email = this.params.data.email;
-    console.log('Delete button clicked for user:', email);
-    // Add your delete logic here
-  }
+    onDeleteButtonClick() {
+        const email = this.params.data.email;
+        console.log('Delete button clicked for user:', email);
+        // Add your delete logic here
+    }
 }

b9c6aebbaa1898a1009c48c405ef18acd8657289
Bouchaib <u20420740@tuks.co.za>
2024-06-21 21:49:56 +0200

    feat(team): Added colors to role confirmation box

--- src/app/pages/team/role-change-confirmation-dialog.component.ts
+++ src/app/pages/team/role-change-confirmation-dialog.component.ts
@@ -5,32 +5,50 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-role-change-confirmation-dialog',
-  standalone: true,
-  imports: [MatDialogModule, MatButtonModule, CommonModule],
-  template: `
-    <h2 mat-dialog-title>Confirm Role Change</h2>
-    <div mat-dialog-content>
-      <p>{{ data.given_name }} {{ data.family_name }}: {{ data.email }}</p>
-      <p>Are you sure you want to change this user's role from {{ data.oldRole }} to {{ data.newRole }}?</p>
-    </div>
-    <div mat-dialog-actions>
-      <button mat-button (click)="onNoClick()">Cancel</button>
-      <button mat-button color="primary" (click)="onYesClick()">Confirm</button>
-    </div>
-  `,
+    selector: 'app-role-change-confirmation-dialog',
+    standalone: true,
+    imports: [MatDialogModule, MatButtonModule, CommonModule],
+    template: `
+        <h2 mat-dialog-title>Confirm Role Change</h2>
+        <div mat-dialog-content>
+            <p>{{ data.given_name }} {{ data.family_name }}: {{ data.email }}</p>
+            <p>
+                Are you sure you want to change this user's role from
+                <strong class="old-role">{{ data.oldRole }}</strong> to
+                <strong class="new-role">{{ data.newRole }}</strong
+                >?
+            </p>
+        </div>
+        <div mat-dialog-actions>
+            <button mat-button (click)="onNoClick()">Cancel</button>
+            <button mat-button color="primary" (click)="onYesClick()">Confirm</button>
+        </div>
+    `,
+    styles: [
+        `
+            .old-role {
+                color: #9c27b0;
+                font-weight: bold;
+            }
+            .new-role {
+                color: #009688;
+                font-weight: bold;
+            }
+        `,
+    ],
 })
 export class RoleChangeConfirmationDialogComponent {
-  constructor(
-    public dialogRef: MatDialogRef<RoleChangeConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { given_name: string, family_name: string, email: string, oldRole: string, newRole: string }
-  ) {}
+    constructor(
+        public dialogRef: MatDialogRef<RoleChangeConfirmationDialogComponent>,
+        @Inject(MAT_DIALOG_DATA)
+        public data: { given_name: string; family_name: string; email: string; oldRole: string; newRole: string }
+    ) {}
 
-  onNoClick(): void {
-    this.dialogRef.close(false);
-  }
+    onNoClick(): void {
+        this.dialogRef.close(false);
+    }
 
-  onYesClick(): void {
-    this.dialogRef.close(true);
-  }
+    onYesClick(): void {
+        this.dialogRef.close(true);
+    }
 }

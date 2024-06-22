953fca2f43813838b81f02ba4c4f04c43edeac7b
tristanU21442615 <u21442615@tuks.co.za>
2024-06-22 10:32:23 +0200

    update

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { TitleService } from '../../components/header/title.service';
 
 @Component({
-  selector: 'app-reports',
-  standalone: true,
-  imports: [],
-  templateUrl: './reports.component.html',
-  styleUrl: './reports.component.css'
+    selector: 'app-reports',
+    standalone: true,
+    imports: [],
+    templateUrl: './reports.component.html',
+    styleUrl: './reports.component.css',
 })
 export class ReportsComponent {
-
+    constructor(private titleService: TitleService) {}
+    ngOnInit() {
+        this.titleService.updateTitle('Reports');
+    }
 }

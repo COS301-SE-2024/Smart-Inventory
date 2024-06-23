d6e24ee661f4c799f88bb42cd7aee0290e90e5dc
tristanU21442615 <u21442615@tuks.co.za>
2024-06-23 11:04:15 +0200

    Finalised help

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -61,61 +61,129 @@ export class HelpComponent implements OnInit {
         { title: 'Contact Support', content: ' ' },
     ];
 
-    faqFilters: string[] = ['General', 'Account Management', 'Inventory Management', 'Notifications'];
+    faqFilters: string[] = ['General', 'Dashboard', 'Inventory', 'Team', 'Suppliers', 'Orders'];
 
     faqs: FAQ[] = [
         {
             question: 'How to reset my password?',
-            answer: 'To reset your password, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Account Management',
+            answer: 'To reset your password, navigate to settings in profile you will see change password under the password section. Click on the "Change Password" button and fill the required details to change your password.',
+            category: 'General',
         },
         {
-            question: 'How to manage inventory?',
-            answer: 'To manage inventory, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Inventory Management',
+            question: 'How remember my password on sign in?',
+            answer: 'To remember a forgotten password, is currently not possible but you can replace it with a new password. Navigate to login page by logging out and clicking forget password and you will receive an email with verification code to change it.',
+            category: 'General',
         },
         {
-            question: 'How to enable notifications?',
-            answer: 'To enable notifications, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Notifications',
+            question: 'How to change my email?',
+            answer: 'To change your email, navigate to settings in profile you will see email under "Details" section. Click on the "Change Email" button and fill the required details to change your email.',
+            category: 'General',
         },
         {
-            question: 'How to reset my password?',
-            answer: 'To reset your password, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Account Management',
+            question: 'How to change my name?',
+            answer: 'To change your name, navigate to settings in profile you will see name input box under "Details" section. Click on the type in the new name to change your name.',
+            category: 'General',
         },
         {
-            question: 'How to manage inventory?',
-            answer: 'To manage inventory, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Inventory Management',
+            question: 'How to add inventory?',
+            answer: 'To add an inventory item on the inventory page, click the "Quick Actions" button and a dropdown menu will appear Select "Add Item" opening a pop-up window. Fill in the details. The pop-up will display all relevant column names alongside input boxes. Enter the necessary information for each field. Click submit. Once you have filled in the details, complete the action to create the new entry. This might involve a button labeled "Save," "Submit," or something similar.',
+            category: 'Inventory',
         },
         {
-            question: 'How to enable notifications?',
-            answer: 'To enable notifications, go to...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-            category: 'Notifications',
+            question: 'How to remove inventory?',
+            answer: 'To remove an inventory item on the inventory page, Firstly you must, left click on row that you want to remove. Click the "Quick Actions" button. A dropdown menu will appear. Select "Remove Item." This opens a pop-up window to verify your delete action. Agree to the pop up now the entry should be removed.',
+            category: 'Inventory',
+        },
+        {
+            question: 'How to edit inventory?',
+            answer: 'To edit an inventory item on the inventory page, To edit any entry in the able it is as simple as double clicking on the cell you want to change. When you have finished editing the cells you press enter to finalize the changes.',
+            category: 'Inventory',
+        },
+        {
+            question: 'How to move widget?',
+            answer: 'To move a widget on dashboard page, You can customize position of each widget by dragging and dropping, click and hold a widget, then drag it to a new location on the dashboard to rearrange the layout for better visibility or workflow.',
+            category: 'Dashboard',
+        },
+        {
+            question: 'How to add widget?',
+            answer: 'To add a widget to the dashboard page, click the "Quick Actions" button in which will give you a drop down menu, now choose the "add widget" menu option. Choose select the type of widget you want and it should place it at the bottom of the page.',
+            category: 'Dashboard',
+        },
+        {
+            question: 'How to remove widget?',
+            answer: 'To remove a widget inventory, click on a widget and while highlighted click on quick actions and choose the delete option.',
+            category: 'Dashboard',
+        },
+        {
+            question: 'How to add member to my team?',
+            answer: 'To add member to my team on the team page,  click the "Quick Actions" button and a dropdown menu will appear Select "Add Member" opening a pop-up window. Fill in the details. The pop-up will display all relevant column names alongside input boxes. Enter the necessary information for each field. Click submit. Once you have filled in the details, complete the action to create the new entry. This might involve a button labeled "Save," "Submit," or something similar.',
+            category: 'Team',
+        },
+        {
+            question: 'How to remove member from my team?',
+            answer: 'To remove a member from my team on the team page, select the delete in the corresponding row of the person you want to remove. It will ask you if you are sure just click yes.',
+            category: 'Team',
+        },
+        {
+            question: 'How to edit member of my teams details?',
+            answer: 'To edit a member of my team on the team page, To edit any entry in the able it is as simple as double clicking on the cell you want to change. When you have finished editing the cells you press enter to finalize the changes.',
+            category: 'Team',
+        },
+        {
+            question: 'How to add a supplier?',
+            answer: 'To add a supplier to supplier page, click the "Quick Actions" button and a dropdown menu will appear Select "Add Supplier" opening a pop-up window. Fill in the details. The pop-up will display all relevant column names alongside input boxes. Enter the necessary information for each field. Click submit. Once you have filled in the details, complete the action to create the new entry. This might involve a button labeled "Save," "Submit," or something similar.',
+            category: 'Suppliers',
+        },
+        {
+            question: 'How to remove a supplier?',
+            answer: 'To remove a supplier from supplier page, Firstly you must, left click on row that you want to remove. Click the "Quick Actions" button. A dropdown menu will appear. Select "Remove Item." This opens a pop-up window to verify your delete action. Agree to the pop up now the entry should be removed.',
+            category: 'Suppliers',
+        },
+        {
+            question: 'How to edit supplier details?',
+            answer: 'To edit supplier details on suppliers page, To edit any entry in the able it is as simple as double clicking on the cell you want to change. When you have finished editing the cells you press enter to finalize the changes.',
+            category: 'Suppliers',
+        },
+        {
+            question: 'How to cancel orders?',
+            answer: 'To enable orders page,',
+            category: 'Orders',
         },
     ];
 
     troubleshootingIssues: TroubleshootingIssue[] = [
         {
-            title: 'Login Issues and Password Reset',
+            title: 'Cannot login',
             description:
-                'Description for Login Issues and Password Reset...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
+                'Firstly make sure you have a good network connection. Then make sure that you are providing the correct details and that the password is correct. If you are sure your details are correct and it still tells you your password is incorrect click on forgot password. If you are still having issues please contact us.',
         },
         {
-            title: 'Notification Issues and Configuration',
+            title: 'Forget password verification code not sent',
             description:
-                'Description for Notification Issues and Configuration...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
+                'If the current screen says a code has been sent. Check that you are looking at the correct email that it would send it to. Also check that you have internet connection. If it is still not there ask to resend the code by clicking on "resend code". If you are still having issues please contact us.',
         },
         {
-            title: 'Stock Level Errors',
+            title: 'Cannot create an account',
             description:
-                'Description for Stock Level Errors...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
+                'If you have been provided with a login link there is no need to create an account as your admin created your account you only need to verify your password with your email. If you are an admin and cannot create an account check that if you have already created one you are not using the same email. Any further issues please contact us.',
+        },
+        {
+            title: 'Cannot add or remove from tables',
+            description:
+                'Firstly if you are and End-User you have no ability to do so. If you are an Inventory controller or admin you should be able to check that you are using the functionality correctly, to find out about functionality go to FAQ or User Guides. Any other issues please contact us.',
+        },
+        {
+            title: 'Cannot add or remove team member',
+            description:
+                'Firstly if you are an End-User or Inventory controller you have no ability to do so. If you are an admin you should be able to check that you are using the functionality correctly, to find out about functionality go to FAQ or User Guides. Any other issues please contact us.',
         },
         {
             title: 'Error Codes',
-            description:
-                'Description for Error Codes...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
+            description: '404: Server is down or not found',
+        },
+        {
+            title: 'Bugs',
+            description: 'If you have identified a bug please do contact us so we can fix it.',
         },
     ];
 

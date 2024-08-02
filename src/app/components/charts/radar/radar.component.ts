0be6eed9a65d5689c1dad85891fd6ceb2e9d9901
Thabang Kgaladi <u21686875@tuks.co.za>
2024-08-02 10:20:22 +0200

    "Updated grid component to dynamically adjust height based on row count, modified supplier report component to display additional columns and data, and made minor formatting changes to various files."

---
+++ src/app/components/charts/radar/radar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radar',
+  standalone: true,
+  imports: [],
+  templateUrl: './radar.component.html',
+  styleUrl: './radar.component.css'
+})
+export class RadarComponent {
+
+}

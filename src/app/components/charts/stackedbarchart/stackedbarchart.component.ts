21ebd48385aaa046ed9192e3488687ae3972340c
Thabang Kgaladi <u21686875@tuks.co.za>
2024-08-07 19:23:23 +0200

    "Added new properties and methods to OrderReportComponent, including calculation of metrics, fetching of orders and supplier quotes, and preparation of chart data."

---
+++ src/app/components/charts/stackedbarchart/stackedbarchart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stackedbarchart',
+  standalone: true,
+  imports: [],
+  templateUrl: './stackedbarchart.component.html',
+  styleUrl: './stackedbarchart.component.css'
+})
+export class StackedbarchartComponent {
+
+}

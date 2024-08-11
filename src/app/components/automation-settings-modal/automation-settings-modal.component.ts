40438fdadefb1cbb4490a2b78632d2e6c980341b
bchraf3 <135054721+bchraf3@users.noreply.github.com>
2024-08-11 09:49:15 +0200

    feat(orders): Added modal for automation settings

---
+++ src/app/components/automation-settings-modal/automation-settings-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-automation-settings-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './automation-settings-modal.component.html',
+  styleUrl: './automation-settings-modal.component.css'
+})
+export class AutomationSettingsModalComponent {
+
+}

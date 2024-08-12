c82f8a1c1e444fa55c967cb652a797a00abb0ea1
HawaIbrahim <148062991+HawaIbr01@users.noreply.github.com>
2024-08-12 06:44:42 +0200

    feat(tests): E2E dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,34 +1,41 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-    viewportWidth: 1000,
-    viewportHeight: 660,
-    component: {
-        devServer: {
-            framework: 'angular',
-            bundler: 'webpack',
-            options: {
-                projectConfig: {
-                    root: '',
-                    sourceRoot: 'src',
-                    buildOptions: {
-                        outputPath: 'dist',
-                        index: 'src/index.html',
-                        main: 'src/main.ts',
-                        polyfills: ['zone.js'],
-                        tsConfig: 'tsconfig.app.json',
-                        assets: ['src/assets'],
-                        styles: [
-                            'node_modules/ag-grid-community/styles/ag-grid.css',
-                            'node_modules/ag-grid-community/styles/ag-theme-alpine.css',
-                            'node_modules/@aws-amplify/ui-angular/theme.css',
-                            'src/styles.css',
-                        ],
-                        scripts: [],
-                    },
-                },
-            },
+  viewportWidth: 1000,
+  viewportHeight: 660,
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+      options: {
+        projectConfig: {
+          root: "",
+          sourceRoot: "src",
+          buildOptions: {
+            outputPath: "dist",
+            index: "src/index.html",
+            main: "src/main.ts",
+            polyfills: ["zone.js"],
+            tsConfig: "tsconfig.app.json",
+            assets: ["src/assets"],
+            styles: [
+              "node_modules/ag-grid-community/styles/ag-grid.css",
+              "node_modules/ag-grid-community/styles/ag-theme-alpine.css",
+              "node_modules/@aws-amplify/ui-angular/theme.css",
+              "src/styles.css",
+            ],
+            scripts: [],
+          },
         },
-        specPattern: '**/*.cy.ts',
+      },
     },
+    specPattern: "**/*.cy.ts",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });

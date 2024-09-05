d326b5d1299715ae4005151b6450573de7559e75
bchraf3 <135054721+bchraf3@users.noreply.github.com>
2024-09-05 13:21:05 +0200

    feat(inventory): Created component for inventory summary page

---
+++ src/app/pages/inventory-summary/inventory-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-summary.component.html',
+  styleUrl: './inventory-summary.component.css'
+})
+export class InventorySummaryComponent {
+
+}

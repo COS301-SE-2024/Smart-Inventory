82b3ae47ca5e9011b80e5d88c6e8575f9a148683
bchraf3 <135054721+bchraf3@users.noreply.github.com>
2024-09-08 12:32:08 +0200

    feat(orders): Added modal for supplier renegotiation

---
+++ src/app/components/supplier-renegotation-modal/supplier-renegotation-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-renegotation-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './supplier-renegotation-modal.component.html',
+  styleUrl: './supplier-renegotation-modal.component.css'
+})
+export class SupplierRenegotationModalComponent {
+
+}

8ded2095bc08c5171a73ce864f63f18a0f43e801
bchraf3 <135054721+bchraf3@users.noreply.github.com>
2024-09-19 22:42:24 +0200

    fear(QRScanning): Added scan-qrcode-modal Modal

---
+++ src/app/components/scan-qrcode-modal/scan-qrcode-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan-qrcode-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './scan-qrcode-modal.component.html',
+  styleUrl: './scan-qrcode-modal.component.css'
+})
+export class ScanQrcodeModalComponent {
+
+}

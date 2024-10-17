03dd560c7644eea5290801391c7752ea30174da3
Thabang Kgaladi <u21686875@tuks.co.za>
2024-10-17 16:02:53 +0200

    Updated

--- src/app/api-key.interceptor.ts
+++ src/app/api-key.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../assets/environments/environment';
+import { environment } from '../environments/environment';
 
 export function apiKeyInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const apiReq = req.clone({

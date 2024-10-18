a97e60e957bda4d12affdee34f4c36eb354430a7
Thabang Kgaladi <u21686875@tuks.co.za>
2024-10-18 16:19:29 +0200

    Updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 // app.config.ts
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiKeyInterceptor } from './api-key.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideAnimationsAsync(),
-        provideHttpClient(withInterceptors([apiKeyInterceptor])),
+        provideHttpClient(withInterceptors([apiKeyInterceptor])), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
     ],
 };
